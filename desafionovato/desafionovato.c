2f66a12a6de410a2fcbde7c7dceb5aa8ae6ec50c
JoaoMarcos1397 <johnmarksmiranda@gmail.com>
2025-09-28 14:00:21 +0000

    decalaração das variáveis

---
+++ desafionovato/desafionovato.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    char letraestado01, letraestado02;
+    char codigo01[10], codigo02[10];
+    char cidade01[50], cidade02[50];
+    int habitantes01, habitantes02;
+    float area01, area02;
+    float pib01, pib02;
+    int turisticos01, turisticos02;
+
+
+
+
+
+
+
+}
